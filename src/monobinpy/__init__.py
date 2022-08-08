5b0cb1094ebecfd8921e31aa03129fb86db527fa
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2022-08-08 07:25:39 +0200

    Update __init__.py

--- src/monobinpy/__init__.py
+++ src/monobinpy/__init__.py
@@ -1,3 +1,12 @@
+"""Performs monotonic binning of numeric risk factor in credit rating models (PD, LGD, EAD)
+	development. All functions handle both binary and continuous target variable.
+	Functions that use isotonic regression in the first stage of binning process have an additional 
+	feature for correction of minimum percentage of observations and minimum target rate per bin.	
+	Additionally, monotonic trend can be identified based on raw data or, if known in advance,
+	forced by functions' argument. Missing values and other possible special values are treated
+	separately from so-called complete cases.
+	This is replica of R 'monobin' package."""
+
 from .pct_bin import pct_bin 
 from .cum_bin import cum_bin
 from .iso_bin import iso_bin
